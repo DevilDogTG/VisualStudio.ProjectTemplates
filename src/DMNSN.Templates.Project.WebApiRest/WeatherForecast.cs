dc5f10385de480a8975d740163a2932bb001707b
Supawat Tanmanee <supawat.tanmanee@elysiannxt.com>
2025-09-26 17:46:13 +0700

    wip: initial webAPI (REST) template

---
+++ src/DMNSN.Templates.Project.WebApiRest/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace DMNSN.Templates.Project.WebApiRest
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
