7e9aab8a5546d81747c05f0ff6814e81e0adce17
Supawat Tanmanee <supawat.tanmanee@elysiannxt.com>
2025-10-01 16:39:54 +0700

    feat: introduce web api (REST) template

---
+++ src/DMNSN.Templates.Project.WebApiRest/Abstracts/DefaultControllerBase.cs
@@ -0,0 +1,68 @@
+﻿using DMNSN.Templates.Project.WebApiRest.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+
+namespace DMNSN.Templates.Project.WebApiRest.Abstracts;
+
+/// <summary>
+/// A default base controller with common properties and methods.
+/// </summary>
+/// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
+public abstract class DefaultControllerBase : ControllerBase
+{
+    protected readonly ILogger _logger;
+    protected readonly IHttpContextAccessor _httpContextAccessor;
+    protected readonly string correlationID;
+    protected AppSettings _config;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DefaultControllerBase"/> class.
+    /// </summary>
+    /// <param name="logger">The logger.</param>
+    /// <param name="httpContextAccessor">The HTTP context accessor.</param>
+    /// <param name="config">The configuration.</param>
+    protected DefaultControllerBase(
+        ILogger logger,
+        IHttpContextAccessor httpContextAccessor,
+        IOptionsMonitor<AppSettings> config)
+    {
+        _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
+        _config = config.CurrentValue;
+        config.OnChange(newValues => _config = newValues);
+        if (_httpContextAccessor.HttpContext != null)
+        { correlationID = (string?)_httpContextAccessor.HttpContext.Request.Headers[_config.CorrelationKey] ?? ""; }
+        else { correlationID = ""; }
+    }
+
+    /// <summary>
+    /// Gets the raw request.
+    /// </summary>
+    /// <returns></returns>
+    protected string GetRawRequest()
+    {
+        Request.Body.Position = 0;
+        var bodyStream = new StreamReader(Request.Body);
+        return bodyStream.ReadToEndAsync().Result;
+    }
+
+    /// <summary>
+    /// Gets the application version.
+    /// </summary>
+    /// <returns></returns>
+    protected static string? GetAppVersion()
+    {
+        string version;
+        var appVersion = Assembly.GetExecutingAssembly().GetName().Version;
+        if (appVersion != null)
+        { version = $"{appVersion.Major}.{appVersion.Minor}.{appVersion.Build}"; }
+        else
+        {
+            var versionAttribute = Assembly.GetExecutingAssembly()
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            version = versionAttribute?.InformationalVersion ?? "x.x.x";
+        }
+        return version;
+    }
+}
---
+++ src/DMNSN.Templates.Project.WebApiRest/Controllers/ExampleController.cs
@@ -0,0 +1,71 @@
+﻿using DMNSN.Templates.Project.WebApiRest.Abstracts;
+using DMNSN.Templates.Project.WebApiRest.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace DMNSN.Templates.Project.WebApiRest.Controllers;
+
+/// <summary>
+/// Example controller for demonstration purposes.
+/// </summary>
+/// <seealso cref="DefaultControllerBase" />
+[Route("api/example")]
+public class ExampleController(
+    ILogger<ExampleController> logger,
+    IHttpContextAccessor httpContextAccessor,
+    IOptionsMonitor<AppSettings> config) : DefaultControllerBase(logger, httpContextAccessor, config)
+{
+    /// <summary>
+    /// Gets the example.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public IActionResult GetExample()
+    {
+        _logger.LogInformation("Example action called. Correlation ID: {CorrelationID}", correlationID);
+        var response = new
+        {
+            Message = "This is an example response.",
+            CorrelationID = correlationID,
+            AppVersion = GetAppVersion()
+        };
+        return Ok(response);
+    }
+
+    /// <summary>
+    /// Posts the echo.
+    /// </summary>
+    /// <returns></returns>
+    [HttpPost("echo")]
+    public IActionResult PostEcho()
+    {
+        var rawRequest = GetRawRequest();
+        _logger.LogInformation("Echo action called. Correlation ID: {CorrelationID}. Raw Request: {RawRequest}", correlationID, rawRequest);
+        var response = new
+        {
+            Message = "Echoing back your request.",
+            CorrelationID = correlationID,
+            RawRequest = rawRequest,
+            AppVersion = GetAppVersion()
+        };
+        return Ok(response);
+    }
+
+    /// <summary>
+    /// Gets the version.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("version")]
+    public IActionResult GetVersion()
+    {
+        var version = GetAppVersion();
+        _logger.LogInformation("Version action called. Correlation ID: {CorrelationID}. App Version: {AppVersion}", correlationID, version);
+        var response = new
+        {
+            Message = "Application version retrieved.",
+            CorrelationID = correlationID,
+            AppVersion = version
+        };
+        return Ok(response);
+    }
+}
