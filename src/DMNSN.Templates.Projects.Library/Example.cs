7849bcd893b0133fe4b08458707038ac886f2f6c
Supawat Tanmanee <supawat.tanmanee@elysiannxt.com>
2025-10-09 11:58:03 +0700

    feat: example helper class

--- src/DMNSN.Templates.Projects.Library/Example.cs
+++ src/DMNSN.Templates.Projects.Library/Example.cs
@@ -1,13 +1,20 @@
-﻿namespace DMNSN.Templates.Projects.Library;
+﻿using Microsoft.Extensions.Logging;
+
+namespace DMNSN.Templates.Projects.Library;
 
 /// <summary>
 /// This is an example class in the library project.
 /// </summary>
-public class Example
+public class Example(
+    ILogger<Example> logger)
 {
     /// <summary>
     /// Gets the message.
     /// </summary>
     /// <returns></returns>
-    public string GetMessage(string name) => $"Hello {name}, this is message from DMNSN.Templates.Projects.Library!";
+    public string GetMessage(string name)
+    {
+        logger.LogInformation("GetMessage called with name: {Name}", name);
+        return $"Hello {name}, this is message from DMNSN.Templates.Projects.Library!";
+    }
 }
---
+++ src/DMNSN.Templates.Projects.Library/Helpers/ExampleHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace DMNSN.Templates.Projects.Library.Helpers;
+
+public static class ExampleHelper
+{
+    public static string GetExample(string message, ILogger? logger)
+    {
+        logger?.LogInformation("GetExample called with message: {Message}", message);
+        return $"This is an example helper method. Input message is {message}";
+    }
+}
